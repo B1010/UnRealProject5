0c0ca30d6047f22e8c871e43311b52f0eca61440
B1010 <artemik-98@outlook.com>
2017-05-05 18:24:50 +0400

    Добавьте файлы проекта.

---
+++ UnRealProject5/Form1.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UnRealProject5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public int cputhread()
+        {
+            RegistryKey threadcount = Registry.LocalMachine.OpenSubKey(@"HARDWARE\DESCRIPTION\System\CentralProcessor");
+            int realthreadcount = threadcount.SubKeyCount;
+            return realthreadcount;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = Convert.ToString(cputhread());
+        }
+    }
+}
